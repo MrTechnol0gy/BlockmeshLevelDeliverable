234ff774bb5ab1bae5a647e8b69f370fd57c8749
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-11-25 11:29:00 -0400

    zone two maintenance sheds complete

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerScript : MonoBehaviour
+{    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject = transform.position(1, 1, 1);
+            Debug.Log("This works.");
+        }
+    }
+
+
+
+
+}
